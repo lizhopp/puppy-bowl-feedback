051af63c14c2bcb6b17139a9c2d5bd0b897a564e
hp-nyc <helenjpark@gmail.com>
2026-04-06 20:18:15 -0400

    modify API link for my cohort and specific end point

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // === Constants ===
-const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
+const BASE = "https://fsa-puppy-bowl.herokuapp.com/api/";
+const COHORT = "2511-HELEN/"; // Make sure to change this!
 const API = BASE + COHORT;
